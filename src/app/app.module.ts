16db65c8ed1c830f11c72eba31afea54b895587b
Airto Cabral <samurai@Airtos-MacBook-Pro.local>
2024-06-29 17:47:28 -0300

    finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MaterialShareModule } from './shared/material-share/material-share.module';
+import { provideHttpClient } from '@angular/common/http';
+import { IMaskModule } from 'angular-imask';
 
 @NgModule({
   declarations: [
@@ -10,11 +14,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialShareModule,
+    ReactiveFormsModule,
+    IMaskModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideHttpClient()
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
+
 export class AppModule { }
